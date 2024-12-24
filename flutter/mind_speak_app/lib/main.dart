b496f8b18a89fe34a6938541719407659e781796
fares abdelhamid <fares2100380@miuegypt.edu.eg>
2024-12-24 21:53:51 +0200

    .

--- flutter/mind_speak_app/lib/main.dart
+++ flutter/mind_speak_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
- 
+import 'package:mind_speak_app/signup.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(); // Initialize Firebase
@@ -8,6 +9,8 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,19 +18,21 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: SignUp(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Mind Speak App"),
+        title: const Text("Mind Speak App"),
       ),
-      body: Center(
+      body: const Center(
         child: Text("Firebase Connected!"),
       ),
     );

3d73a1e18fa0d9fed2ac6a2a358a0e5417c1730e
ahmed9949 <aosma9949@gamil.com>
2024-12-29 10:44:59 +0200

    session apge
    empty

---
+++ flutter/mind_speak_app/lib/start_session.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StartSession extends StatefulWidget {
+  const StartSession({super.key});
+
+  @override
+  State<StartSession> createState() => _StartSessionState();
+}
+
+class _StartSessionState extends State<StartSession> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

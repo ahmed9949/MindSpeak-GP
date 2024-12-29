c27599722774caa3a6063898db34adeb24ff3f9d
ahmed9949 <aosma9949@gamil.com>
2024-12-29 10:37:12 +0200

    adding assets
    adding assets , components for parent interface and choosing avatar

---
+++ flutter/mind_speak_app/lib/components/homepageCards.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class smallcard extends StatelessWidget {
+  smallcard({required this.color, required this.text});
+  final Color color;
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 8.0),
+      child: Container(
+        width: 125,
+        height: 125,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BiggerCard extends StatelessWidget {
+  const BiggerCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+              width: double.infinity, // Take full width
+              height: 200, // Bigger height
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: const Center(
+                child: Text(
+                  'Big Card',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24, // Bigger font size
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            );
+  }
+}

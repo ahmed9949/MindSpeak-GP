03bf91ff41343a6be2f199249e7ce8ae3258e47b
Ahmedhossam26 <125680149+Ahmedhossam26@users.noreply.github.com>
2025-01-06 18:39:44 +0200

    making the cards in the dashboard dynamic

--- flutter/mind_speak_app/lib/pages/DashBoard.dart
+++ flutter/mind_speak_app/lib/pages/DashBoard.dart
@@ -1,10 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:mind_speak_app/components/CustomBottomNavigationBar.dart';
 import 'package:mind_speak_app/providers/theme_provider.dart';
+import 'package:mind_speak_app/service/database.dart';
+
 import 'package:provider/provider.dart';
 
-
- 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -25,91 +26,51 @@ class DashBoard extends StatefulWidget {
 }
 
 class _DashBoardState extends State<DashBoard> {
-  int usersCount = 10;
-  int doctorsCount = 25;
+  int usersCount = 0;
+  int therapistCount = 0;
 
   bool showUsersCount = false;
-  bool showDoctorsCount = false;
+  bool showTherapistCount = false;
 
-  List<Map<String, String>> users = [
-    {
-      'name': 'Ahmed',
-      'email': 'ahmed@gmail.com',
-      'phone': '01028200287',
-      'nationalId': '1234567890'
-    },
-    {
-      'name': 'Mohamed',
-      'email': 'mohamed@gmail.com',
-      'phone': '01028200287',
-      'nationalId': '1234567891'
-    },
-    {
-      'name': 'Osos',
-      'email': 'osos@gmail.com',
-      'phone': '01028200287',
-      'nationalId': '1234567892'
-    },
-    {
-      'name': 'Fares',
-      'email': 'fares@gmail.com',
-      'phone': '01028200287',
-      'nationalId': '1234567893'
-    },
-    {
-      'name': 'Ali',
-      'email': 'ali@gmail.com',
-      'phone': '01028200287',
-      'nationalId': '1234567894'
-    },
-    {
-      'name': 'Sara',
-      'email': 'sara@gmail.com',
-      'phone': '01028200287',
-      'nationalId': '1234567895'
-    },
-    {
-      'name': 'Nour',
-      'email': 'nour@gmail.com',
-      'phone': '01028200287',
-      'nationalId': '1234567896'
-    },
-    {
-      'name': 'Laila',
-      'email': 'laila@gmail.com',
-      'phone': '01028200287',
-      'nationalId': '1234567897'
-    },
-    {
-      'name': 'Youssef',
-      'email': 'youssef@gmail.com',
-      'phone': '01028200287',
-      'nationalId': '1234567898'
-    },
-    {
-      'name': 'Khaled',
-      'email': 'khaled@gmail.com',
-      'phone': '01028200287',
-      'nationalId': '1234567899'
-    }
-  ];
+  final DatabaseMethods databaseMethods = DatabaseMethods();
+  List<Map<String, String>> users = [];
 
   int currentPage = 0;
   int itemsPerPage = 5;
 
   @override
-  Widget build(BuildContext context) {
-            final themeProvider = Provider.of<ThemeProvider>(context);
+  void initState() {
+    super.initState();
+    fetchCounts();
+  }
 
+  Future<void> fetchCounts() async {
+    try {
+      int users = await databaseMethods.getUsersCount();
+      int therapists = await databaseMethods.getTherapistsCount();
+      setState(() {
+        usersCount = users;
+        therapistCount = therapists;
+      });
+    } catch (e) {
+      print('Error fetching counts: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
     int totalPages = (users.length / itemsPerPage).ceil();
 
     return Scaffold(
       appBar: AppBar(
-         actions: [
+        actions: [
           IconButton(
-            icon: Icon(themeProvider.isDarkMode ? Icons.wb_sunny : Icons.nightlight_round),
+            icon: Icon(themeProvider.isDarkMode
+                ? Icons.wb_sunny
+                : Icons.nightlight_round),
             onPressed: () {
-              themeProvider.toggleTheme(); // Toggle the theme
+              themeProvider.toggleTheme();
             },
           ),
         ],
@@ -120,166 +81,162 @@ class _DashBoardState extends State<DashBoard> {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: OrientationBuilder(
-        builder: (context, orientation) {
-          return SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Card(
-                    elevation: 5,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Card(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
                         children: [
-                          Column(
-                            children: [
-                              GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    showUsersCount = !showUsersCount;
-                                  });
-                                },
-                                child: const Icon(Icons.person,
-                                    size: 40, color: Colors.blue),
-                              ),
-                              const SizedBox(height: 5),
-                              const Text(
-                                "Users",
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              if (showUsersCount)
-                                Text(
-                                  "$usersCount",
-                                  style: const TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.black,
-                                  ),
-                                ),
-                            ],
+                          GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                showUsersCount = !showUsersCount;
+                              });
+                            },
+                            child: const Icon(Icons.person,
+                                size: 40, color: Colors.blue),
                           ),
-                          Column(
-                            children: [
-                              GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    showDoctorsCount = !showDoctorsCount;
-                                  });
-                                },
-                                child: const Icon(Icons.medical_services,
-                                    size: 40, color: Colors.green),
-                              ),
-                              const SizedBox(height: 5),
-                              const Text(
-                                "Doctors",
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              if (showDoctorsCount)
-                                Text(
-                                  "$doctorsCount",
-                                  style: const TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.black,
-                                  ),
-                                ),
-                            ],
+                          const SizedBox(height: 5),
+                          const Text(
+                            "Users",
+                            style: TextStyle(fontWeight: FontWeight.bold),
                           ),
+                          if (showUsersCount)
+                            Text(
+                              "$usersCount",
+                              style: const TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                              ),
+                            ),
                         ],
                       ),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  const Center(
-                    child: Text(
-                      'Doctors Requests',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: DataTable(
-                      columns: const [
-                        DataColumn(label: Text('Name')),
-                        DataColumn(label: Text('Email')),
-                        DataColumn(label: Text('Mobile')),
-                        DataColumn(label: Text('National ID')),
-                        DataColumn(label: Text('Actions')),
-                      ],
-                      rows: users
-                          .skip(currentPage * itemsPerPage)
-                          .take(itemsPerPage)
-                          .map((user) => DataRow(cells: [
-                                DataCell(Text(user['name']!)),
-                                DataCell(Text(user['email']!)),
-                                DataCell(Text(user['phone']!)),
-                                DataCell(Text(user['nationalId']!)),
-                                DataCell(Row(
-                                  children: [
-                                    IconButton(
-                                      icon: const Icon(Icons.check,
-                                          color: Colors.green),
-                                      onPressed: () {
-                                        setState(() {
-                                          users.remove(user);
-                                        });
-                                      },
-                                    ),
-                                    IconButton(
-                                      icon: const Icon(Icons.close,
-                                          color: Colors.red),
-                                      onPressed: () {
-                                        setState(() {
-                                          users.remove(user);
-                                        });
-                                      },
-                                    ),
-                                  ],
-                                ))
-                              ]))
-                          .toList(),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.arrow_back),
-                        onPressed: currentPage > 0
-                            ? () {
-                                setState(() {
-                                  currentPage--;
-                                });
-                              }
-                            : null,
-                      ),
-                      Text('${currentPage + 1} / $totalPages'),
-                      IconButton(
-                        icon: const Icon(Icons.arrow_forward),
-                        onPressed: currentPage < totalPages - 1
-                            ? () {
-                                setState(() {
-                                  currentPage++;
-                                });
-                              }
-                            : null,
+                      Column(
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                showTherapistCount = !showTherapistCount;
+                              });
+                            },
+                            child: const Icon(Icons.medical_services,
+                                size: 40, color: Colors.green),
+                          ),
+                          const SizedBox(height: 5),
+                          const Text(
+                            "Therapists",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          if (showTherapistCount)
+                            Text(
+                              "$therapistCount",
+                              style: const TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                              ),
+                            ),
+                        ],
                       ),
                     ],
                   ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Center(
+                child: Text(
+                  'Therapist Requests',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: DataTable(
+                  columns: const [
+                    DataColumn(label: Text('Name')),
+                    DataColumn(label: Text('Email')),
+                    DataColumn(label: Text('Mobile')),
+                    DataColumn(label: Text('National ID')),
+                    DataColumn(label: Text('Actions')),
+                  ],
+                  rows: users
+                      .skip(currentPage * itemsPerPage)
+                      .take(itemsPerPage)
+                      .map((user) => DataRow(cells: [
+                            DataCell(Text(user['name']!)),
+                            DataCell(Text(user['email']!)),
+                            DataCell(Text(user['phone']!)),
+                            DataCell(Text(user['nationalId']!)),
+                            DataCell(Row(
+                              children: [
+                                IconButton(
+                                  icon: const Icon(Icons.check,
+                                      color: Colors.green),
+                                  onPressed: () {
+                                    setState(() {
+                                      users.remove(user);
+                                    });
+                                  },
+                                ),
+                                IconButton(
+                                  icon: const Icon(Icons.close,
+                                      color: Colors.red),
+                                  onPressed: () {
+                                    setState(() {
+                                      users.remove(user);
+                                    });
+                                  },
+                                ),
+                              ],
+                            ))
+                          ]))
+                      .toList(),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back),
+                    onPressed: currentPage > 0
+                        ? () {
+                            setState(() {
+                              currentPage--;
+                            });
+                          }
+                        : null,
+                  ),
+                  Text('${currentPage + 1} / $totalPages'),
+                  IconButton(
+                    icon: const Icon(Icons.arrow_forward),
+                    onPressed: currentPage < totalPages - 1
+                        ? () {
+                            setState(() {
+                              currentPage++;
+                            });
+                          }
+                        : null,
+                  ),
                 ],
               ),
-            ),
-          );
-        },
+            ],
+          ),
+        ),
       ),
       bottomNavigationBar: const CustomBottomNavigationBar(currentIndex: 0),
     );
--- flutter/mind_speak_app/lib/service/database.dart
+++ flutter/mind_speak_app/lib/service/database.dart
@@ -1,7 +1,36 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class DatabaseMethods{
-  Future addUser(String userId, Map<String, dynamic>  userInfoMap){
-    return FirebaseFirestore.instance.collection("User").doc(userId).set(userInfoMap);
+class DatabaseMethods {
+  // hena 3ashn inject el function fel 7eta ely 3ayzha fel project dependencies injection
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future addUser(String userId, Map<String, dynamic> userInfoMap) {
+    return FirebaseFirestore.instance
+        .collection("User")
+        .doc(userId)
+        .set(userInfoMap);
+  }
+
+
+  // hena function 3ashn a count number of users fel system
+  Future<int> getUsersCount() async {
+    try {
+      QuerySnapshot snapshot = await _firestore.collection('user').get();
+      return snapshot.size;
+    } catch (e) {
+      throw Exception('Failed to fetch users count $e');
+    }
+  }
+
+  // hena function 3ashn a count number of therapists fel system
+  Future<int> getTherapistsCount() async {
+    try{
+      QuerySnapshot snapshot = await _firestore.collection('therapist').get();
+      return snapshot.size;
+    }
+
+    catch (e) {
+      throw Exception('Failed to fetch therapists count $e');
+    }
   }
 }

99ebf81633f65a2492a37c352dd24bf6754e9c11
Ahmedhossam26 <125680149+Ahmedhossam26@users.noreply.github.com>
2025-01-14 02:53:21 +0200

    add search and fetch Therapist from firebase

--- flutter/mind_speak_app/lib/pages/ViewDoctorPage.dart
+++ flutter/mind_speak_app/lib/pages/ViewDoctorPage.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:mind_speak_app/providers/theme_provider.dart';
 import 'package:provider/provider.dart';
@@ -8,20 +9,24 @@ class ViewDoctorsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-            final themeProvider = Provider.of<ThemeProvider>(context);
+    final themeProvider = Provider.of<ThemeProvider>(context);
 
     return Scaffold(
       appBar: AppBar(
         actions: [
           IconButton(
-            icon: Icon(themeProvider.isDarkMode ? Icons.wb_sunny : Icons.nightlight_round),
+            icon: Icon(themeProvider.isDarkMode
+                ? Icons.wb_sunny
+                : Icons.nightlight_round),
             onPressed: () {
               themeProvider.toggleTheme(); // Toggle the theme
             },
           ),
         ],
         centerTitle: true,
-      backgroundColor: themeProvider.isDarkMode ? Colors.black : Colors.lightBlue, // Custom color for this screen
+        backgroundColor: themeProvider.isDarkMode
+            ? Colors.black
+            : Colors.lightBlue, // Custom color for this screen
         title: const Text(
           "View Doctors",
           style: TextStyle(color: Colors.white),
@@ -41,82 +46,150 @@ class ViewDoctors extends StatefulWidget {
 }
 
 class _ViewDoctorsState extends State<ViewDoctors> {
-  final List<Map<String, dynamic>> _allDoctors = [
-    {"id": 1, "name": "Ahmed", "Email": "ahmed@gmail.com"},
-    {"id": 2, "name": "Fares", "Email": "fares@gmail.com"},
-    {"id": 3, "name": "Osos", "Email": "osos@gmail.com"},
-    {"id": 4, "name": "Mohamed", "Email": "mohamed@gmail.com"}
-  ];
+  List<Map<String, dynamic>> _allTherapist = [];
+  List<Map<String, dynamic>> _foundTherapist = [];
+  bool _isLoading = true;
 
-  List<Map<String, dynamic>> _foundUsers = [];
   @override
   void initState() {
-    _foundUsers = _allDoctors;
     super.initState();
+    fetchApprovedTherapist();
+  }
+
+  // hena 3ashn get all therapist el approved fel system
+  Future<void> fetchApprovedTherapist() async {
+    try {
+      // Fetch all approved therapists
+      QuerySnapshot therapistSnapshot = await FirebaseFirestore.instance
+          .collection('therapist')
+          .where('status', isEqualTo: true)
+          .get();
+
+      // Get all user IDs from the fetched therapists
+      List<String> userIds =
+          therapistSnapshot.docs.map((doc) => doc['userid'] as String).toList();
+
+      // Fetch all user documents in a single batch call
+      QuerySnapshot userSnapshot = await FirebaseFirestore.instance
+          .collection('user')
+          .where(FieldPath.documentId, whereIn: userIds)
+          .get();
+
+      // Create a map of userId to user data for quick lookup
+      Map<String, Map<String, dynamic>> userMap = {
+        for (var doc in userSnapshot.docs)
+          doc.id: doc.data() as Map<String, dynamic>
+      };
+
+      // Combine therapist data with user data
+      List<Map<String, dynamic>> loadedTherapists = therapistSnapshot.docs.map(
+        (doc) {
+          var therapistData = doc.data() as Map<String, dynamic>;
+          var userData = userMap[therapistData['userid']] ?? {};
+
+          return {
+            'id': doc.id,
+            'name': userData['username'] ?? 'N/A',
+            'email': userData['email'] ?? 'N/A',
+            'nationalid': therapistData['nationalid'] ?? 'N/A',
+          };
+        },
+      ).toList();
+
+      setState(() {
+        _allTherapist = loadedTherapists;
+        _foundTherapist = loadedTherapists;
+        _isLoading = false;
+      });
+    } catch (e) {
+      print('Error fetching therapists: $e');
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Error fetching therapists: $e'),
+        backgroundColor: Colors.red,
+      ));
+      setState(() {
+        _isLoading = false;
+      });
+    }
   }
 
   void _runFilter(String searchNames) {
     List<Map<String, dynamic>> results = [];
     if (searchNames.isEmpty) {
-      results = _allDoctors;
+      results = _allTherapist;
     } else {
-      results = _allDoctors
+      results = _allTherapist
           .where((user) =>
               user["name"].toLowerCase().contains(searchNames.toLowerCase()))
           .toList();
     }
 
     setState(() {
-      _foundUsers = results;
+      _foundTherapist = results;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          TextField(
-            onChanged: (value) => _runFilter(value),
-            decoration: const InputDecoration(
-              labelText: 'Search',
-              suffixIcon: Icon(Icons.search),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _foundUsers.length,
-              itemBuilder: (context, index) => Card(
-                key: ValueKey(_foundUsers[index]["id"].toString()),
-                color: Colors.blue,
-                elevation: 4,
-                margin: const EdgeInsets.symmetric(vertical: 10),
-                child: ListTile(
-                  leading: Text(
-                    _foundUsers[index]["id"].toString(),
-                    style: const TextStyle(fontSize: 24, color: Colors.white),
-                  ),
-                  title: Text(
-                    _foundUsers[index]['name'],
-                    style: const TextStyle(color: Colors.white),
-                  ),
-                  subtitle: Text(
-                    _foundUsers[index]['Email'],
-                    style: const TextStyle(color: Colors.white),
+    return _isLoading
+        ? const Center(child: CircularProgressIndicator())
+        : Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                TextField(
+                  onChanged: (value) => _runFilter(value),
+                  decoration: const InputDecoration(
+                    labelText: 'Search',
+                    suffixIcon: Icon(Icons.search),
                   ),
                 ),
-              ),
+                const SizedBox(height: 20),
+                Expanded(
+                  child: _foundTherapist.isNotEmpty
+                      ? ListView.builder(
+                          itemCount: _foundTherapist.length,
+                          itemBuilder: (context, index) => Card(
+                            key: ValueKey(_foundTherapist[index]["id"]),
+                            color: Colors.blue,
+                            elevation: 4,
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                            child: ListTile(
+                              leading: Text(
+                                (index + 1).toString(),
+                                style: const TextStyle(
+                                    fontSize: 24, color: Colors.white),
+                              ),
+                              title: Text(
+                                _foundTherapist[index]['name'],
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Email: ${_foundTherapist[index]['email']}',
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
+                                  Text(
+                                    'National ID: ${_foundTherapist[index]['nationalid']}',
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        )
+                      : const Center(
+                          child: Text(
+                            'No approved doctors found.',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
-    );
+          );
   }
 }

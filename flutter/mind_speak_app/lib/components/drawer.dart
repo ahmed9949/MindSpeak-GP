0e679c389ef3d048828bc3a5a6214312d730acdd
mohamedhussammm <mohamed2110201@miuegypt.edu.eg>
2025-01-18 22:31:23 +0200

    autism prediction ai model

--- flutter/mind_speak_app/lib/components/drawer.dart
+++ flutter/mind_speak_app/lib/components/drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mind_speak_app/pages/homepage.dart';
 import 'package:mind_speak_app/pages/logout.dart';
+import 'package:mind_speak_app/pages/predict.dart';
 import 'package:mind_speak_app/pages/profilepage.dart';
 
 class NavigationDrawe extends StatelessWidget {
@@ -47,6 +48,15 @@ class NavigationDrawe extends StatelessWidget {
             title: const Text('Settings'),
             onTap: () {
               Navigator.pop(context);
+            },
+          ),
+           ListTile(
+            leading: const Icon(Icons.mobile_screen_share),
+            title: const Text('Prediction'),
+           onTap: () {
+              Navigator.pop(context);
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Predict()));
             },
           ),
           ListTile(
